e5227e5970fb7a3af16d09039f4def0c98a5718b
Josh Pollock <josh@calderawp.com>
2019-03-27 10:46:32 -0400

    Add more props and inline docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,53 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
-
+import moment from 'moment';
 import './styles.css';
 
-interface PostContent {
+/**
+ * Interface for post title, content and excerpt
+ */
+interface ContentObject {
+	//This property is always present
 	rendered: string;
+	//This property is only present in some contexts
+	raw?: string;
 }
 
-interface PostTitle {
-	rendered: string;
-}
+/**
+ * Interface for describing post title
+ */
+interface PostTitle extends ContentObject {}
+/**
+ * Interface for describing post content
+ */
+interface PostContent extends ContentObject {}
+/**
+ * Interface for describing post content
+ */
+interface PostExcerpt extends ContentObject {}
 
+/**
+ * Interface descrinbing a WordPress post
+ */
 interface Post {
 	title: PostTitle;
 	content: PostContent;
+	excerpt: PostExcerpt;
+	date: string;
+	id: Number;
 }
 
+/**
+ * Display A Blog Post
+ *
+ * @param props
+ */
 const BlogPost = (props: { post: Post }) => {
 	const { post } = props;
 	return (
-		<article>
+		<article id={`post-${post.id}`}>
 			<h1>{post.title.rendered}</h1>
+			<p>Published {moment(post.date).fromNow()}</p>
 			<p>{post.content.rendered}</p>
 		</article>
 	);
@@ -28,12 +55,17 @@ const BlogPost = (props: { post: Post }) => {
 
 function App() {
 	const post: Post = {
+		id: 42,
 		title: {
 			rendered: 'Post Title'
 		},
 		content: {
 			rendered: 'Post Content'
-		}
+		},
+		excerpt: {
+			rendered: 'Post Excerpt'
+		},
+		date: '2019-03-19T06:53:51'
 	};
 	return (
 		<div className="App">
