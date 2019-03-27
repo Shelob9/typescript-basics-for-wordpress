20ed0f4335a28d597bb098ba1b427fe8d16e7293
Josh Pollock <josh@calderawp.com>
2019-03-27 11:05:49 -0400

    Add example of managing state

--- src/index.tsx
+++ src/index.tsx
@@ -42,13 +42,19 @@ interface Post {
  *
  * @param props
  */
-const BlogPost = (props: { post: Post }) => {
-	const { post } = props;
+const BlogPost = (props: { post: Post; showContent: boolean }) => {
+	const { post, showContent } = props;
 	return (
 		<article id={`post-${post.id}`}>
 			<h1>{post.title.rendered}</h1>
 			<p>Published {moment(post.date).fromNow()}</p>
-			<p>{post.content.rendered}</p>
+			<div>
+				{showContent ? (
+					<Fragment>{post.content.rendered}</Fragment>
+				) : (
+					<Fragment>{post.excerpt.rendered}</Fragment>
+				)}
+			</div>
 		</article>
 	);
 };
@@ -58,18 +64,36 @@ const BlogPost = (props: { post: Post }) => {
  *
  * @param props
  */
-const ListOfPosts = (props: { posts: Array<Post> }) => {
-	const { posts } = props;
+const ListOfPosts = (props: {
+	posts: Array<Post>;
+	showContent: boolean;
+	toggleShowContent: (enable: boolean) => void;
+}) => {
+	const { posts, showContent, toggleShowContent } = props;
 	return (
-		<Fragment>
-			{posts.map(post => (
-				<BlogPost key={post.id} post={post} />
-			))}
-		</Fragment>
+		<div>
+			<button
+				onClick={e => {
+					e.preventDefault();
+					toggleShowContent(!showContent);
+				}}>
+				Show Full Content
+			</button>
+			<Fragment>
+				{posts.map(post => (
+					<BlogPost key={post.id} post={post} showContent={showContent} />
+				))}
+			</Fragment>
+		</div>
 	);
 };
 
 function App() {
+	/**
+	 * State of content vs excerpt display
+	 * Putting it here to demonstrate passing change handlers
+	 */
+	const [showContent, toggleShowContent] = useState<boolean | null>(null);
 	const post: Post = {
 		id: 42,
 		title: {
@@ -83,9 +107,26 @@ function App() {
 		},
 		date: '2019-03-19T06:53:51'
 	};
+	const post2: Post = {
+		id: 4000,
+		title: {
+			rendered: 'Post Two'
+		},
+		content: {
+			rendered: 'Post 2 Content'
+		},
+		excerpt: {
+			rendered: 'Post 2 Excerpt'
+		},
+		date: '2019-01-19T06:53:51'
+	};
 	return (
 		<div className="App">
-			<ListOfPosts posts={[post]} />
+			<ListOfPosts
+				posts={[post, post2]}
+				showContent={showContent}
+				toggleShowContent={toggleShowContent}
+			/>
 		</div>
 	);
 }
